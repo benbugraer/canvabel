654f574dfb7414b735b75027b6d3b84dbc13c431
benbugraer <bugraerdev@gmail.com>
2024-08-26 00:40:43 +0300

    refactor: Update DashboardLayout and DashboardNav components
    Refactor the DashboardLayout component to include a sidebar and navigation bar for better organization and usability. Update the layout structure to use CSS grid for responsive design.
    
    In the DashboardNav component, add a header with a logo and menu icon for mobile navigation. Implement a sheet component for the menu to slide in from the left side. Add buttons for different sections of the dashboard.
    
    These changes enhance the dashboard's layout and navigation, improving the user experience.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,10 @@ import { CSSProperties } from "react";
 export default async function Dashboard() {
   return (
     <div className="flex min-w-screen p-4 flex-col items-center justify-between w-full">
-      <div className="flex flex-col gap-2 lg:gap-2 min-h-[88vh] w-full">
+      <div
+        className="flex flex-col gap-2 lg:gap-2 min-h-[88vh] w-full animate-in"
+        style={{ "--index": 5 } as CSSProperties}
+      >
         <div className="flex flex-1 items-center justify-center rounded-lg border border-primary shadow-sm">
           <div className="flex flex-col items-center text-center"></div>
         </div>
